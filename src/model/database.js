5bfd095c80aa5e0d7a4673e367aa500d95782187
Sanjay Bisu <sanjayb.iitkgp@gmail.com>
2023-03-17 00:33:41 +0530

    database link

--- src/model/database.js
+++ src/model/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const db = "mongodb://localhost:27017/bishu"
+const db = "mongodb+srv://bishu:8Dr0Cvs4zncTM0Of@cluster0.uul8ovz.mongodb.net/bisu?retryWrites=true&w=majority"
 mongoose.connect(db,{
     useNewUrlParser :true,
 }).then(()=>{
