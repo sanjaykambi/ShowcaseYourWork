2d82fb58442eb2583e1e7e2a645953c14f9c52c7
Ved Prakash <vedprakash182001@gmail.com>
2023-03-18 21:13:45 +0530

    d

--- src/model/database.js
+++ src/model/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const db = "mongodb+srv://bishu:8Dr0Cvs4zncTM0Of@cluster0.uul8ovz.mongodb.net/bisu?retryWrites=true&w=majority"
 const db1 = "mongodb://localhost:27017/bishu"
-mongoose.connect(db1,{
+mongoose.connect(db,{
     useNewUrlParser :true,
 }).then(()=>{
     console.log("Connection Successfull")
